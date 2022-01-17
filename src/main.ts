7f4b2be1b123b09a6841edb3d8a4a70bd47c4ee4
RenanCrisConceicao <renancris21@gmail.com>
2022-01-17 19:12:22 -0300

    condigurações

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
     module.hot.accept();
